478b40c423ae690ffe09c45dab54e0ef949e388b
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-02-02 18:09:12 -0700

    updating core code and tests

--- vulture_whitelist.py
+++ vulture_whitelist.py
@@ -1,5 +1,3 @@
-# This whitelist file tells Vulture that these functions are intentionally used
-# even if they are only referenced from outside the src/ tree (e.g., tests).
+# Whitelist for Vulture: mark these names as used externally
 
-def construct_simulation_state():
-    pass
+construct_simulation_state
