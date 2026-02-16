870159895663872ce27df5b58b0dc61872896dd6
Dmitrii Zavalin's Aerospace & IT Projects <135647339+Dmitrii-Zavalin-Deployments@users.noreply.github.com>
2026-02-15 20:11:48 -0700

    Create test_advection_basic.py

---
+++ tests/tests_physics/advection/test_advection_basic.py
@@ -0,0 +1,13 @@
+"""
+Physics Test: Advection Operator
+--------------------------------
+This test will validate:
+- donor‑cell upwind advection
+- stability under CFL
+- correct transport of a shear layer
+- no spurious oscillations
+"""
+
+def test_advection_basic_placeholder():
+    # TODO: implement advection operator test
+    assert True
