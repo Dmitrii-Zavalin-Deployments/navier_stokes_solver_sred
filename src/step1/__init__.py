54c0e541cbdd71d589ced9cf913447e791c21923
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-02-18 06:40:02 -0700

    fixing unit tests

--- src/step1/__init__.py
+++ src/step1/__init__.py
@@ -1,28 +1,7 @@
 # src/step1/__init__.py
 
-from .parse_config import parse_config
-from .validate_physical_constraints import validate_physical_constraints
-from .initialize_grid import initialize_grid
-from .allocate_fields import allocate_fields
-from .apply_initial_conditions import apply_initial_conditions
-from .map_geometry_mask import map_geometry_mask
-from .compute_derived_constants import compute_derived_constants
-from .verify_cell_centered_shapes import verify_cell_centered_shapes
-from .parse_boundary_conditions import parse_boundary_conditions
-from .assemble_simulation_state import assemble_simulation_state
-from .orchestrate_step1 import orchestrate_step1, orchestrate_step1_state
+from .orchestrate_step1 import orchestrate_step1_state
 
 __all__ = [
-    "parse_config",
-    "validate_physical_constraints",
-    "initialize_grid",
-    "allocate_fields",
-    "apply_initial_conditions",
-    "map_geometry_mask",
-    "compute_derived_constants",
-    "verify_cell_centered_shapes",
-    "parse_boundary_conditions",
-    "assemble_simulation_state",
-    "orchestrate_step1",
     "orchestrate_step1_state",
 ]
