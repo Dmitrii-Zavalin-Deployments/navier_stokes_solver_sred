cc9325bbbb332753bbf09125975e67aac35869a3
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-02-18 06:13:05 -0700

    fixing unit tests

--- src/step1/__init__.py
+++ src/step1/__init__.py
@@ -1,7 +1,6 @@
 # src/step1/__init__.py
 
 from .parse_config import parse_config
-from .validate_json_schema import validate_json_schema
 from .validate_physical_constraints import validate_physical_constraints
 from .initialize_grid import initialize_grid
 from .allocate_fields import allocate_fields
@@ -11,11 +10,10 @@ from .compute_derived_constants import compute_derived_constants
 from .verify_cell_centered_shapes import verify_cell_centered_shapes
 from .parse_boundary_conditions import parse_boundary_conditions
 from .assemble_simulation_state import assemble_simulation_state
-from .orchestrate_step1 import orchestrate_step1
+from .orchestrate_step1 import orchestrate_step1, orchestrate_step1_state
 
 __all__ = [
     "parse_config",
-    "validate_json_schema",
     "validate_physical_constraints",
     "initialize_grid",
     "allocate_fields",
@@ -26,4 +24,5 @@ __all__ = [
     "parse_boundary_conditions",
     "assemble_simulation_state",
     "orchestrate_step1",
+    "orchestrate_step1_state",
 ]
