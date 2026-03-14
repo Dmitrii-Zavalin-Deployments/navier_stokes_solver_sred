d28ff8d8bdce9b76e8850d3e0c3ad7e30780160b
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-03-14 16:43:51 +0000

    🧹 Auto-clean: structural hygiene

--- tests/property_integrity/test_boundary_condition_integrity.py
+++ tests/property_integrity/test_boundary_condition_integrity.py
@@ -1,7 +1,6 @@
 # tests/property_integrity/test_boundary_condition_integrity.py
 
 import pytest
-import numpy as np
 
 from tests.helpers.solver_output_schema_dummy import make_output_schema_dummy
 from tests.helpers.solver_step2_output_dummy import make_step2_output_dummy
